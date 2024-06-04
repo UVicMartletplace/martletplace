9d6f5bd07d1171f656e94f3c3c4c833c975516d2
gavindeane <gavindeane1@gmail.com>
2024-06-04 09:39:39 -0700

    Created dummy API endpoint for GET /api/search

--- apps/algorithm/search/src/server.py
+++ apps/algorithm/search/src/server.py
@@ -1,8 +1,37 @@
-from fastapi import FastAPI, Response
+from fastapi import FastAPI
 
 app = FastAPI()
 
 
 @app.get("/api/search")
-async def home(response: Response):
-    return "Hello World"
+async def search(authorization: str, query: str, latitude: float,
+                 longitude: float, page: int,
+                 limit: int, minPrice: float = None,
+                 maxPrice: float = None, status: str = "AVAILABLE",
+                 searchType: str = "LISTINGS", sort: str = "RELEVANCE"
+                 ):
+    # actual logic will go here
+
+    # This is a dummy response
+    return [
+        {
+            "listingID": "A23F29039B23",
+            "sellerID": "A23F29039B23",
+            "sellerName": "John Doe",
+            "title": "Used Calculus Textbook",
+            "description": "No wear and tear, drop-off available.",
+            "price": 50,
+            "dateCreated": "2024-05-23T15:30:00Z",
+            "imageUrl": "image URL for first Image"
+        },
+        {
+            "listingID": "A23F29039B24",
+            "sellerID": "A23F29039B24",
+            "sellerName": "Jane Doe",
+            "title": "Used Physics Textbook",
+            "description": "No wear and tear, drop-off available.",
+            "price": 40,
+            "dateCreated": "2024-05-23T15:30:00Z",
+            "imageUrl": "image URL for first Image"
+        }
+    ]
