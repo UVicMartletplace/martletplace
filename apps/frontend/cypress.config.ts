975caac16a8ceaf5135ff8c476eee2eaa061667c
Caelum Dudek <caelumdudek25@gmail.com>
2024-06-03 23:23:59 -0700

    Cypress working with web but not in IDE

---
+++ apps/frontend/cypress.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from "cypress";
+import registerCodeCoverageTasks from "@cypress/code-coverage/task";
+
+export default defineConfig({
+  // setupNodeEvents can be defined in either
+  // the e2e or component configuration
+  e2e: {
+    specPattern: "tests/e2e_tests/*.cy.{js,jsx,ts,tsx}",
+    setupNodeEvents(on, config) {
+      registerCodeCoverageTasks(on, config);
+    },
+  },
+
+  component: {
+    specPattern: "tests/component_tests/*.cy.{js,jsx,ts,tsx}",
+    devServer: {
+      framework: "react",
+      bundler: "vite",
+    },
+    setupNodeEvents(on, config) {
+      registerCodeCoverageTasks(on, config);
+      return config;
+    }
+  },
+});
---
+++ apps/frontend/tests/component_tests/App.cy.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import App from '../../src/App'
+import * as cypress from "cypress";
+
+describe('<App />', () => {
+  it('renders', () => {
+    // see: https://on.cypress.io/mounting-react
+    cy.mount(<App />)
+  })
+})
