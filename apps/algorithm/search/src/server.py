36d7710773851225fa692c997f3a38c8c814bd0b
gavindeane <gavindeane1@gmail.com>
2024-06-04 11:01:44 -0700

    Added stub methods for listing-created, listing-edited, and listing-deleted along with a pydantic schematic for the Listing object to pass in.

--- apps/algorithm/search/src/server.py
+++ apps/algorithm/search/src/server.py
@@ -1,18 +1,22 @@
-from pydantic import BaseModel, Field
-from typing import List, Optional
 from enum import Enum
+from typing import List
+
 from fastapi import FastAPI
+from pydantic import BaseModel, Field
 
 app = FastAPI()
+
 
 # define enums
 class Status(str, Enum):
     AVAILABLE = "AVAILABLE"
     SOLD = "SOLD"
 
+
 class SearchType(str, Enum):
     LISTINGS = "LISTINGS"
     SELLERS = "SELLERS"
+
 
 class Sort(str, Enum):
     RELEVANCE = "RELEVANCE"
@@ -23,7 +27,8 @@ class Sort(str, Enum):
     DISTANCE_ASC = "DISTANCE_ASC"
     DISTANCE_DESC = "DISTANCE_DESC"
 
-# Define Pydantic model
+
+# Define Pydantic models
 class ListingSummary(BaseModel):
     listingID: str = Field(..., example="A23F29039B23")
     sellerID: str = Field(..., example="A23F29039B23")
@@ -35,13 +40,30 @@ class ListingSummary(BaseModel):
     dateCreated: str = Field(..., example="2024-05-23T15:30:00Z")
     imageUrl: str = Field(..., example="image URL for first Image")
 
+
+class Listing(BaseModel):
+    listingId: str = Field(..., example="abc123")
+    sellerId: str = Field(..., example="seller456")
+    sellerName: str = Field(..., example="billybobjoe")
+    title: str = Field(..., example="High-Performance Laptop")
+    description: str = Field(...,
+                             example="A powerful laptop suitable for gaming "
+                                     "and professional use.")
+    price: float = Field(..., example=450.00)
+    location: dict = Field(...,
+                           example={"latitude": 45.4215, "longitude": -75.6972})
+    status: str = Field(..., example="AVAILABLE")
+    dateCreated: str = Field(..., example="2024-05-22T10:30:00Z")
+    imageUrl: str = Field(..., example="https://example.com/image1.jpg")
+
+
 @app.get("/api/search", response_model=List[ListingSummary])
 async def search(
-    authorization: str, query: str, latitude: float,
-    longitude: float, page: int, limit: int,
-    minPrice: float = None, maxPrice: float = None,
-    status: str = "AVAILABLE", searchType: str = "LISTINGS",
-    sort: str = "RELEVANCE"
+        authorization: str, query: str, latitude: float,
+        longitude: float, page: int, limit: int,
+        minPrice: float = None, maxPrice: float = None,
+        status: str = "AVAILABLE", searchType: str = "LISTINGS",
+        sort: str = "RELEVANCE"
 ):
     # actual logic will go here
 
@@ -68,3 +90,24 @@ async def search(
             "imageUrl": "image URL for first Image"
         }
     ]
+
+
+@app.post("/api/search/reindex/listing-created")
+async def reindex_listing_created(authorization: str, listing: Listing):
+    # actual logic will go here
+
+    return {"message": "Listing added successfully."}
+
+
+@app.patch("/api/search/reindex/listing-edited")
+async def reindex_listing_edited(authorization: str, listing: Listing):
+    # actual logic will go here
+
+    return {"message": "Listing edited successfully."}
+
+
+@app.delete("/api/search/reindex/listing-deleted")
+async def reindex_listing_deleted(authorization: str, listingId: str):
+    # actual logic will go here
+
+    return {"message": "Listing deleted successfully."}
