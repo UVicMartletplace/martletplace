8f216f9a0b6f2d3137a429cb912b11f62dff43cd
Sejal Agarwal <sejalagarwal@Sejals-MacBook-Air.local>
2024-06-04 19:36:28 -0700

    create files for each page

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,7 +1,36 @@
-import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import Login from "./pages/Login.tsx";
+import ForgotPassword from "./pages/ForgotPassword";
+import CreateAccount from "./pages/CreateAccount";
+import Homepage from "./pages/Homepage";
+import CreateListing from "./pages/CreateListing";
+import EditListing from "./pages/EditListing";
+import Account from "./pages/Account";
+import ViewListing from "./pages/ViewListing";
+import Messages from "./pages/Messages";
 
 function App() {
-  return <h1>Martletplace</h1>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/create-account" element={<CreateAccount />} />
+        <Route path="/homepage" element={<Homepage />} />
+        <Route path="/create-listing" element={<CreateListing />} />
+        <Route path="/edit-listing/:id" element={<EditListing />} />
+        <Route path="/account" element={<Account />} />
+        <Route path="/view-listing/:id" element={ViewListing} />
+        <Route path="/messages" element={<Messages />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
