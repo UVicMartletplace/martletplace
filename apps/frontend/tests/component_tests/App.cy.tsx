f829555b215c42a5944f7b101aba829aa22765be
caelumd25 <caelumdudek25@gmail.com>
2024-06-04 20:00:58 -0700

    Fixed the linting and testing(hopefully)

--- apps/frontend/tests/component_tests/App.cy.tsx
+++ apps/frontend/tests/component_tests/App.cy.tsx
@@ -1,7 +1,5 @@
 import App from '../../src/App'
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import * as cypress from "cypress";
-// Note when testing this run it as a --component
+
 
 describe('<App />', () => {
   it('renders', () => {
