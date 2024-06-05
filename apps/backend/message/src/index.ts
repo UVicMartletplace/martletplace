d7a23acf7eef85c3372b617967e7ddee8b932784
Toby Murray <tobystefan@gmail.com>
2024-06-04 17:17:58 -0700

    RESOPS30 - Set up messages CI

--- apps/backend/message/src/index.ts
+++ apps/backend/message/src/index.ts
@@ -7,15 +7,16 @@ const app = express();
 app.use(morgan("dev"));
 app.use(express.json());
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-    console.error(err.stack);
-    res.status(500).send("Something went wrong");
+  console.error(err.stack);
+  res.status(500).send("Something went wrong");
 });
 
 app.get("/api/message", (req: Request, res: Response) => {
-    res.send("Hello world");
+  res.send("Hello world");
 });
 
 app.listen(PORT, "0.0.0.0", () => {
-    console.log(`Server running at http://0.0.0.0:${PORT}`);
+  console.log(`Server running at http://0.0.0.0:${PORT}`);
 });
