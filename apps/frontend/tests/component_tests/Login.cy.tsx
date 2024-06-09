955d8c8e8921a1aed1cca9ba1fbe1e9bd56e8eab
sejal-agarwal <72957712+sejal-agarwal@users.noreply.github.com>
2024-06-08 17:36:15 -0700

    Sejal/frontend67/69 Create Account and Login Pages (#143)
    # Description
    <!-- Please include a summary of the change(s) and which issue is being
    fixed. Please provide as much detail as possible. -->
    Added pages for `Create Account` and `Login` as well as tests. I wasn't
    able to use the Cypress GUI for some reason so I had to write the tests
    from scratch. There may be quite a few errors there, I apologize in
    advance. 🫣
    
    ## Images
    
    ### Login
    **Desktop**
    <img width="1267" alt="Screenshot 2024-06-08 at 12 37 39 PM"
    src="https://github.com/UVicMartletplace/martletplace/assets/72957712/2164d4b2-4872-4391-8860-bb1d74ce1add">
    
    **Mobile**
    <img width="849" alt="Screenshot 2024-06-08 at 12 38 28 PM"
    src="https://github.com/UVicMartletplace/martletplace/assets/72957712/9d807cec-ff66-45a1-af29-cb79366e5502">
    
    ### Create Account
    
    
    **Desktop**
    <img width="1266" alt="Screenshot 2024-06-08 at 12 39 19 PM"
    src="https://github.com/UVicMartletplace/martletplace/assets/72957712/9b3d3823-da6d-4fc8-bd3b-c1562a17cdf7">
    
    **Mobile**
    <img width="855" alt="Screenshot 2024-06-08 at 12 39 01 PM"
    src="https://github.com/UVicMartletplace/martletplace/assets/72957712/77b29d7d-7c12-4393-9943-027ef3cba029">
    
    
    I created a colors file with standard colours for our application. 
    
    Also added a file for styling components that might be helpful though I
    do still have quite a bit inline styling as well.
    
    <!-- Replace `XXX` with the concerning issue number. The "#" links this
    PR to its relevant issue -->
    Closes #67 
    Closes #69 
    
    ## How to Test
    <!-- Provide some simple steps so that others can verify your
    implementation -->
    Login
    - [ ] Run application locally
    - [ ] Valid Login: Login with any credentials and you should be able to
    navigate to the homepage.
    - [ ] Invalid Login: Comment out line 33 in `Login.tsx` to see a failed
    login attempt
    
    Create Account
    - [ ] Run application locally
    - [ ] Test by not using a valid email format (ie. missing the "@") -
    should get an error message
    - [ ] Test with and without a uvic account
    - [ ] Try creating an account with and without a valid password
    - [ ] Valid Account Creation: Creating an account with valid parameters
    will be successful and lead you to the homepage
    - [ ] Invalid Account Creation: Comment out line 71 is
    `CreateAccount.tsx` to see a failed account creation attempt
    
    
    ## Checklist
    - [x] The code includes tests if relevant
    - [x] I have *actually* self-reviewed my changes and done QA
    <!-- Only check this off if you have actually done a self-review! DO NOT
    request any review from others until you have done your self-review! -->
    
    ---------
    
    Co-authored-by: Sejal Agarwal <sejalagarwal@Sejals-MacBook-Air.local>
    Co-authored-by: Sejal Agarwal <sejalagarwal@w134-87-173-149.wireless.uvic.ca>

---
+++ apps/frontend/tests/component_tests/Login.cy.tsx
@@ -0,0 +1,114 @@
+import { BrowserRouter } from "react-router-dom";
+import Login from "../../src/pages/Login";
+
+describe("<Login />", () => {
+  beforeEach(() => {
+    cy.mount(
+      <BrowserRouter>
+        <Login />
+      </BrowserRouter>
+    );
+  });
+
+  it("renders the login form", () => {
+    cy.get("form").should("be.visible");
+
+    // Check if the form contains the necessary input fields
+    cy.get('input[type="text"]').should("be.visible");
+    cy.get('input[type="password"]').should("be.visible");
+    cy.get('button[type="submit"]').should("be.visible");
+  });
+
+  const testEmail = "testEmail@uvic.ca";
+  const testPassword = "testPassword";
+
+  it("allows typing into the input fields", () => {
+    // Type into the input fields
+    cy.get('input[type="text"]')
+      .type(testEmail)
+      .should("have.value", testEmail);
+    cy.get('input[type="password"]')
+      .type(testPassword)
+      .should("have.value", testPassword);
+  });
+
+  it("submits the form and navigates on successful login", () => {
+    // Stubbing network request for login as successful
+    cy.intercept("POST", "/api/login", {
+      statusCode: 200,
+      body: { success: true },
+    }).as("loginRequest");
+
+    // Type into the input fields
+    cy.get('input[type="text"]').type(testEmail);
+    cy.get('input[type="password"]').type(testPassword);
+
+    // Ensure the button is not disabled
+    cy.get('button[type="submit"]').should("not.be.disabled").click();
+
+    // Wait for the mock request to complete and check its status
+    cy.wait("@loginRequest").its("response.statusCode").should("eq", 200);
+
+    // Check if navigation occurred as expected
+    cy.location("pathname").should("eq", "/");
+  });
+
+  it("does not navigate on unsuccessful login", () => {
+    // Stubbing network request for login as unsuccessful
+    cy.intercept("POST", "/api/login", {
+      statusCode: 401, // Use an appropriate status code for failed login
+      body: { success: false, message: "Invalid credentials" },
+    }).as("loginFailRequest");
+
+    // Type into the input fields
+    cy.get('input[type="text"]').type("wronguser");
+    cy.get('input[type="password"]').type("wrongpassword");
+
+    // Ensure the button is not disabled
+    cy.get('button[type="submit"]').should("not.be.disabled").click();
+
+    // Wait for the mock request to complete and check its status
+    cy.wait("@loginFailRequest").its("response.statusCode").should("eq", 401);
+
+    // Check if navigation has not occurred, implying the user is still on the login page
+    // Uncomment below once backend is ready: ticket #140
+    // cy.location("pathname").should("eq", "/user/login");
+
+    // Check if the error message is displayed
+    cy.contains(
+      "Login unsuccessful. Invalid username and password combination"
+    ).should("be.visible");
+  });
+
+  it("shows forget password link", () => {
+    cy.contains("Forgot Password?")
+      .should("be.visible")
+      .and("have.attr", "href", "/user/resetpassword");
+  });
+
+  it("shows create account link", () => {
+    cy.contains("New? Create an Account")
+      .should("be.visible")
+      .and("have.attr", "href", "/user/signup");
+  });
+
+  it("prevents submission when username is missing", () => {
+    // Type into the input fields
+    cy.get('input[type="password"]').type(testPassword);
+
+    // Ensure the button is disabled
+    cy.get('button[type="submit"]').should("be.disabled");
+  });
+
+  it("prevents submission when password is missing", () => {
+    // Type into the input fields
+    cy.get('input[type="text"]').type(testEmail);
+
+    // Ensure the button is disabled
+    cy.get('button[type="submit"]').should("be.disabled");
+  });
+
+  it("prevents submission when both fields are empty", () => {
+    cy.get('button[type="submit"]').should("be.disabled");
+  });
+});
