bc6168ea9d29052e608bedfd19beed2f6be31e1d
sejal-agarwal <72957712+sejal-agarwal@users.noreply.github.com>
2024-06-12 11:22:57 -0700

    Account Page Sidebar (#145)
    # Description
    <!-- Please include a summary of the change(s) and which issue is being
    fixed. Please provide as much detail as possible. -->
    Created `Account` page side panel that allows user to navigate to their
    profile page, view their listings, and see listings they have reviewed.
    By default, when the user first arrives on the `Account` page they will
    land on their `Profile` page.
    
    <img width="1269" alt="Screenshot 2024-06-11 at 8 56 55 AM"
    src="https://github.com/UVicMartletplace/martletplace/assets/72957712/023d14f6-a437-424b-8a2a-f757aa66a920">
    
    <!-- Replace `XXX` with the concerning issue number. The "#" links this
    PR to its relevant issue -->
    Closes #61 
    
    ## How to Test
    <!-- Provide some simple steps so that others can verify your
    implementation -->
    - [ ] Run MartletPlace locally
    - [ ] Navigate to url: http://localhost:8101/user
    - [ ] Try clicking on the different options ion the side to ensure you
    can see the Your Profile, Your Listings, and Your Reviews, components
    
    ## Checklist
    - [x] The code includes tests if relevant
    - [x] I have *actually* self-reviewed my changes and done QA
    <!-- Only check this off if you have actually done a self-review! DO NOT
    request any review from others until you have done your self-review! -->
    
    ---------
    
    Co-authored-by: Sejal Agarwal <sejalagarwal@w134-87-173-149.wireless.uvic.ca>

---
+++ apps/frontend/tests/component_tests/AccountSidebar.cy.tsx
@@ -0,0 +1,34 @@
+import { BrowserRouter } from "react-router-dom";
+import MyProfile from "../../src/pages/MyProfile";
+
+describe("Account Page", () => {
+  beforeEach(() => {
+    cy.mount(
+      <BrowserRouter>
+        <MyProfile />
+      </BrowserRouter>
+    );
+    cy.viewport(1280, 720);
+  });
+
+  it("navigates to the My Profile page", () => {
+    cy.get("h5").contains("My Profile").click();
+
+    // Check if the url contains the correct path
+    cy.url().should("include", "/user");
+  });
+
+  it("navigates to the My Listings page", () => {
+    cy.get("h5").contains("My Listings").click();
+
+    // Check if the url contains the correct path
+    cy.url().should("include", "/user/listings");
+  });
+
+  it("navigates to the My Reviews page", () => {
+    cy.get("h5").contains("My Reviews").click();
+
+    // Check if the url contains the correct path
+    cy.url().should("include", "/user/reviews");
+  });
+});
