a8bfe4552d74348367b7954b891e5f1a5a437193
BuildTools <unconfigured@null.spigotmc.org>
2024-06-11 17:07:03 -0700

    Fixed formatting of files for correct idents.

--- apps/algorithm/recommend/src/training/index.py
+++ apps/algorithm/recommend/src/training/index.py
@@ -1,18 +1,22 @@
 import tensorflow as tf
 import pandas as pd
 
+
 def load_data():
-    data = pd.read_csv('amazon.csv')
-    data['user_id'] = data['user_id'].astype(str)
-    data['product_id'] = data['product_id'].astype(str)
-    data['rating'] = data['rating'].astype(float)
+    data = pd.read_csv("amazon.csv")
+    data["user_id"] = data["user_id"].astype(str)
+    data["product_id"] = data["product_id"].astype(str)
+    data["rating"] = data["rating"].astype(float)
     return data
 
+
 def preprocess_data(data):
-    dataset = tf.data.Dataset.from_tensor_slices({
-        'user_id': data['user_id'].values,
-        'item_id': data['product_id'].values,
-        'rating': data['rating'].values
-    })
+    dataset = tf.data.Dataset.from_tensor_slices(
+        {
+            "user_id": data["user_id"].values,
+            "item_id": data["product_id"].values,
+            "rating": data["rating"].values,
+        }
+    )
     dataset = dataset.shuffle(len(data)).batch(128).cache()
     return dataset
--- apps/algorithm/recommend/src/training/train.py
+++ apps/algorithm/recommend/src/training/train.py
@@ -7,12 +7,14 @@ data = load_data()
 dataset = preprocess_data(data)
 
 # Define and compile model
-user_vocab = data['user_id'].unique()
-item_vocab = data['product_id'].unique()
+user_vocab = data["user_id"].unique()
+item_vocab = data["product_id"].unique()
 model = RecommenderModel(user_vocab, item_vocab)
 
 # Build the model by providing the input shape
-model.build(input_shape={'user_id': tf.TensorShape([None]), 'item_id': tf.TensorShape([None])})
+model.build(
+    input_shape={"user_id": tf.TensorShape([None]), "item_id": tf.TensorShape([None])}
+)
 
 # Compile the model
 model.compile(optimizer=tf.keras.optimizers.Adagrad(learning_rate=0.1))
@@ -21,4 +23,4 @@ model.compile(optimizer=tf.keras.optimizers.Adagrad(learning_rate=0.1))
 model.fit(dataset, epochs=500)
 
 # Save the model
-model.save('saved_model/recommender_model')
+model.save("saved_model/recommender_model")
