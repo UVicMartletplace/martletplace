a55651b13a07cfe7d63dde0f2b3628ef45e1125b
Julian France <julian123france@gmail.com>
2024-06-13 11:16:20 -0700

    jest config completed

--- apps/backend/user/jest.config.ts
+++ apps/backend/user/jest.config.ts
@@ -1,5 +1,9 @@
 import type { Config } from 'jest';
 
-const config: Config = {};
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/tests/**/*.test.ts'],
+};
 
 export default config;
