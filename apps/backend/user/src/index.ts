74b130e9c3320ca4928e7a522cb99ec6b4e58897
Julian France <julian123france@gmail.com>
2024-06-13 16:21:23 -0700

    WIP CRUD endpoints

--- apps/backend/user/src/index.ts
+++ apps/backend/user/src/index.ts
@@ -1,11 +1,20 @@
 import express, { Request, Response, NextFunction } from "express";
 import morgan from "morgan";
-
-import { run } from "../../lib/src/example";
+import pgPromise from "pg-promise";
 
 const PORT = 8211;
 
 const app = express();
+const pgp = pgPromise();
+const DB_ENDPOINT = process.env.DB_ENDPOINT;
+
+if (!DB_ENDPOINT) {
+  console.error("DB_ENDPOINT environment variable is not set");
+  process.exit(1);
+}
+
+const db = pgp(DB_ENDPOINT);
+
 app.use(morgan("dev"));
 app.use(express.json());
 
@@ -15,11 +24,162 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   res.status(500).send("Something went wrong");
 });
 
-app.get("/api/user", (req: Request, res: Response) => {
-  run();
-  res.send("Hello world");
-});
+const createUser = async (req: Request, res: Response, next: NextFunction) => {
+  const { username, password, email, name, bio, profile_pic_url } = req.body;
 
-app.listen(PORT, "0.0.0.0", () => {
+  if (!username || !password || !email) {
+    return res.status(400).send("Username, password, and email are required");
+  }
+
+  const query = `
+      INSERT INTO users (username, email, password, name, bio, profile_pic_url, verified)
+      VALUES ($1, $2, $3, $4, $5, $6, $7)
+      RETURNING user_id, username, email, name, bio, profile_pic_url, verified, created_at, modified_at;
+    `;
+
+  const values = [username, password, email, name, bio, profile_pic_url, false];
+
+  await db.oneOrNone(query, values)
+    .then((data: User) => {
+      if (!data) {
+        throw new Error("User not created");
+      }
+
+      return res.status(201).send(data);
+    })
+    .catch((err: Error) => {
+      return next(err);
+    });
+};
+
+const getUserById = async (req: Request, res: Response, next: NextFunction) => {
+  // TODO add JWT auth
+
+  const { id } = req.params;
+
+  const query = `
+      SELECT * from users where user_id = ${id}
+    `;
+
+  await db.oneOrNone(query)
+    .then((data: User) => {
+      if (!data) {
+        return res.status(400).send("User not found");
+      }
+
+      return res.status(200).send(data);
+    })
+    .catch((err: Error) => {
+      return next(err);
+    });
+};
+
+const patchUserById = async (req: Request, res: Response, next: NextFunction) => {
+  // TODO add JWT auth
+
+  const { id } = req.params;
+  const { username, email, name, bio, profile_pic_url, verified } = req.body;
+
+  try {
+    // Get current user data
+    const getUserQuery = `
+      SELECT * FROM users WHERE user_id = $1
+    `;
+    const originalUser = await db.oneOrNone(getUserQuery, [id]);
+
+    if (!originalUser) {
+      return res.status(400).send("User not found");
+    }
+
+    // Construct patched user!
+    const patchedUser = {
+      username: username || originalUser.username,
+      email: email || originalUser.email,
+      name: name || originalUser.name,
+      bio: bio || originalUser.bio,
+      profile_pic_url: profile_pic_url || originalUser.profile_pic_url,
+      verified: verified || originalUser.verified,
+    };
+
+    const patchUserQuery = `
+      UPDATE users
+      SET username = $1, email = $2, name = $3, bio = $4, profile_pic_url = $5, verified = $6
+      WHERE user_id = $7
+      RETURNING user_id, username, email, name, bio, profile_pic_url, verified, created_at, modified_at;
+    `;
+
+    const updatedUser = await db.oneOrNone(patchUserQuery, [
+      patchedUser.username,
+      patchedUser.email,
+      patchedUser.name,
+      patchedUser.bio,
+      patchedUser.profile_pic_url,
+      patchedUser.verified,
+      id,
+    ]);
+
+    if (!updatedUser) {
+      return res.status(400).send("User not updated");
+    }
+
+    return res.status(200).send(updatedUser);
+  } catch (err) {
+    return next(err);
+  }
+};
+
+const deleteUserById = async (req: Request, res: Response, next: NextFunction) => {
+  const authorization = req.headers.authorization;
+  // Change to use auth/JWT
+  // if (!authorization) {
+  //   return res.status(401).send("Unauthorized");
+  // }
+  const { id }  = req.params;
+
+  const query = `
+    DELETE FROM users
+    WHERE user_id = $1
+    RETURNING *;
+  `;
+
+  await db.oneOrNone(query, [id])
+    .then((data) => {
+      if (!data) {
+        return res.status(404).send("User not found");
+      }
+    return res.status(200).send("User deleted successfully");
+    })
+    .catch((err) => {
+      return next(err);
+    });
+};
+
+// Create user
+app.post("/api/user", createUser);
+
+// Get user
+app.get("/api/user/:id", getUserById);
+
+// Patch user
+app.patch("/api/user/:id", patchUserById);
+
+// Delete user
+app.delete("/api/user/:id", deleteUserById);
+
+app.listen(PORT, () => {
   console.log(`Server running at http://0.0.0.0:${PORT}`);
 });
+
+interface User {
+  user_id: number;
+  username: string;
+  email: string;
+  name: string;
+  bio: string;
+  profile_pic_url: string;
+  verified: boolean;
+  created_at: Date;
+  modified_at: Date;
+}
+
+export { app, createUser, getUserById, patchUserById, deleteUserById};
