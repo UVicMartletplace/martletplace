64c084cc2376886b12ceec61e812d1b0ec54a0bc
Caelum Dudek <54737724+CaelumD25@users.noreply.github.com>
2024-06-17 13:16:05 -0700

    Caelum/frontend/72  create individual listings page (#162)
    # Description
    <!-- Please include a summary of the change(s) and which issue is being
    fixed. Please provide as much detail as possible. -->
    In this pull request implements the /listing/new and /listing/view/{id}
    routes. Editing a listing will follow shortly after this is approved as
    create listing and edit listing are essentially identical.
    
    
    ![image](https://github.com/UVicMartletplace/martletplace/assets/54737724/5a59502a-186d-44c9-a0e5-5f312c0804b9)
    
    
    ![image](https://github.com/UVicMartletplace/martletplace/assets/54737724/828903f1-2482-465b-af59-a75f1887ae1e)
    
    There may be an issue with CI where the headless cypress tests fail
    while the GUI ones do not.
    
    <!-- Replace `XXX` with the concerning issue number. The "#" links this
    PR to its relevant issue -->
    Closes 
    - #72
    - #73 
    - #60 
    - #51 
    
    ## How to Test
    <!-- Provide some simple steps so that others can verify your
    implementation -->
    Run `npx cypress run --component` to run tests headless, but the GUI
    works better it can be run by using `npx cypress open`
    
    ## Checklist
    - [x] The code includes tests if relevant
    - [x] I have *actually* self-reviewed my changes and done QA
    <!-- Only check this off if you have actually done a self-review! DO NOT
    request any review from others until you have done your self-review! -->
    
    ---------
    
    Co-authored-by: Sejal Agarwal <sejalagarwal@Sejals-MacBook-Air.local>
    Co-authored-by: Sejal Agarwal <sejalagarwal@w134-87-173-149.wireless.uvic.ca>

--- apps/frontend/src/pages/CreateListing.tsx
+++ apps/frontend/src/pages/CreateListing.tsx
@@ -1,5 +1,366 @@
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  Container,
+  FormControl,
+  FormHelperText,
+  Grid,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { ChangeEvent, FormEventHandler, useState } from "react";
+import _axios_instance from "../_axios_instance.tsx";
+import { colors } from "../styles/colors.tsx";
+import MultiFileUpload from "../components/MultiFileUpload.tsx";
+import Carousel from "../components/Carousel.tsx";
+
+interface ImageURLObject {
+  url: string;
+}
+interface LocationObject {
+  latitude: number;
+  longitude: number;
+}
+
+interface ListingObject {
+  title: string;
+  description: string;
+  price: number;
+  location: LocationObject;
+  images: ImageURLObject[];
+}
+
+interface NewListingObject {
+  listing: ListingObject;
+}
+
 const CreateListing = () => {
-  return <h1>CreateListing</h1>;
+  const [listingImages, setListingImages] = useState<string[]>([]);
+  const [listingImageBinaries, setListingImageBinaries] = useState<string[]>(
+    [],
+  );
+  const [priceError, setPriceError] = useState<string>("");
+  const [titleError, setTitleError] = useState<string>(
+    "This field is required",
+  );
+  const [sent, setSent] = useState(false);
+
+  // This newListingObject bundles all the listing data for upload to the server
+  const [newListingObject, setNewListingObject] = useState<NewListingObject>({
+    listing: {
+      title: "",
+      description: "",
+      price: 0,
+      location: {
+        latitude: 48.463302,
+        longitude: -123.3108,
+      },
+      images: [],
+    },
+  });
+
+  // Updates and sends the newListingObject, to the server via post under /api/listing
+  const handleSubmit: FormEventHandler<HTMLFormElement> = async (
+    submissionEvent,
+  ) => {
+    submissionEvent.preventDefault();
+    console.log("Listing Image Binaries", listingImageBinaries);
+    if (!priceError && !titleError && !sent) {
+      // In order to make sure that the images are retrieved before submitting
+      const successImages: boolean = await asyncListingImageWrapper();
+      const successLocation: boolean = await asyncListingLocationWrapper();
+
+      if (successImages && successLocation) {
+        _axios_instance
+          .post("/listing", newListingObject)
+          .then(() => {
+            alert("Listing Created!");
+            setSent(true);
+          })
+          .catch(() => {
+            alert("Listing Creation Failed");
+            setSent(false);
+          });
+      } else if (!successImages) {
+        alert("Images failed to upload, please try again later");
+        setSent(false);
+      } else {
+        // Currently this is added to catch if the location is not set, we could default this to the location of the university instead
+        alert(
+          "Error occurred when creating a listing, you may need to enable location permissions for this site",
+        );
+        setSent(false);
+      }
+    }
+  };
+
+  const updateNewListingPayload = (
+    key: keyof ListingObject,
+    value: string | number | LocationObject,
+  ) => {
+    if (["title", "description", "price", "location", "images"].includes(key)) {
+      setNewListingObject((prevState) => ({
+        ...prevState,
+        listing: {
+          ...prevState.listing,
+          [key]: value,
+        },
+      }));
+      return newListingObject.listing[key] === value;
+    }
+  };
+
+  const asyncListingImageWrapper = async (): Promise<boolean> => {
+    try {
+      const imagesObjectArray = await asyncUploadImages();
+      if (imagesObjectArray) {
+        const tempListingObject = newListingObject;
+        tempListingObject.listing.images = imagesObjectArray;
+        setNewListingObject((prevState) => ({
+          ...prevState,
+          tempListingObject,
+        }));
+        return true;
+      } else {
+        return false;
+      }
+    } catch (error) {
+      console.error("Error uploading images:", error);
+      return false;
+    }
+  };
+
+  const updateListingTitle = (event: ChangeEvent<HTMLInputElement>) => {
+    // Handle
+    if (!event.target.value) {
+      setTitleError("This field is required");
+    } else {
+      setTitleError("");
+    }
+    updateNewListingPayload("title", event.target.value);
+  };
+
+  const updateListingDescription = (
+    event: ChangeEvent<HTMLTextAreaElement>,
+  ) => {
+    // Handle
+    updateNewListingPayload("description", event.target.value);
+  };
+
+  const updateListingPrice = (event: ChangeEvent<HTMLInputElement>) => {
+    // Handle
+    const regex = /^\d+(.\d{1,2})?$/;
+    if (!regex.test(event.target.value)) {
+      setPriceError(
+        "This price is not valid, please make sure the value is positive and in the form xx.xx",
+      );
+    } else {
+      setPriceError("");
+      const priceValue: number = +event.target.value;
+      updateNewListingPayload(
+        "price",
+        priceValue >= 0 ? priceValue : priceValue * -1,
+      );
+    }
+  };
+
+  // Gets the user location, and adds it to the listing object
+  const updateListingLocation = () => {
+    try {
+      const currentLocation: LocationObject = { latitude: 0, longitude: 0 };
+      navigator.geolocation.getCurrentPosition((position) => {
+        const currentLatitude = position.coords.latitude;
+        const currentLongitude = position.coords.longitude;
+        if (currentLatitude !== 0 && currentLongitude !== 0) {
+          currentLocation.latitude = currentLatitude;
+          currentLocation.longitude = currentLongitude;
+        }
+      });
+      updateNewListingPayload("location", currentLocation);
+      return true;
+    } catch (error) {
+      return false;
+    }
+  };
+
+  const asyncListingLocationWrapper = async (): Promise<boolean> => {
+    try {
+      return updateListingLocation();
+    } catch (error) {
+      return false;
+    }
+  };
+
+  // This function makes sure that the passed in url is a base64 data string
+  const isImageValid = (url: string) => {
+    try {
+      return url.startsWith("data:image/");
+    } catch (error) {
+      return false;
+    }
+  };
+
+  // Uploads the images to the S3 server, this is handled separately
+  const asyncUploadImages = async (): Promise<ImageURLObject[] | false> => {
+    const retrievedImages: ImageURLObject[] = [];
+    // Create an array of promises for image uploads
+    const uploadPromises = listingImageBinaries.map(async (imageBinary) => {
+      try {
+        // Attempt to upload the image
+        const response = await _axios_instance.post("/images", imageBinary, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        });
+        // Return the URL of the uploaded image on success
+        return { url: response.data.url };
+      } catch (error) {
+        // Log error and return null on failure
+        console.error("Error uploading image:", error);
+        return null; // Indicate failure with null
+      }
+    });
+
+    try {
+      // Wait for all upload promises to resolve
+      const results = await Promise.all(uploadPromises);
+
+      // Filter successful uploads and add to retrievedImages
+      results.forEach((result) => {
+        if (result) {
+          retrievedImages.push(result);
+        }
+      });
+
+      // Return the list of successfully uploaded images
+      return retrievedImages;
+    } catch (error) {
+      // Handle any errors that occurred during the uploading process
+      console.error("Error in uploading image process:", error);
+      return false;
+    }
+  };
+
+  // Upload button html
+  const buttonHTML = (
+    <span style={{ textAlign: "center" }}>
+      <Button
+        className="btn-choose"
+        variant="outlined"
+        component="span"
+        sx={{
+          mt: 2,
+          textTransform: "none",
+          fontSize: "16px",
+          padding: "10px 20px",
+          margin: "10px",
+        }}
+      >
+        Choose Images
+      </Button>
+    </span>
+  );
+
+  return (
+    <Container>
+      <Card>
+        <CardContent>
+          <Typography variant={"h2"}>Create Listing</Typography>
+          <Grid container spacing={1}>
+            <Grid item md={6} sm={12} xs={12}>
+              <Box>
+                <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+                  <FormControl sx={{ width: "100%", padding: "10px" }}>
+                    <TextField
+                      id="field-title"
+                      label="Title"
+                      sx={{ m: "10px" }}
+                      rows={1}
+                      onChange={updateListingTitle}
+                      required
+                      error={!!titleError}
+                    />
+                    {titleError && (
+                      <FormHelperText error>{titleError}</FormHelperText>
+                    )}
+                    <TextField
+                      id="field-description"
+                      label="Description"
+                      type="text"
+                      sx={{ m: "10px", display: "flex" }}
+                      rows={10}
+                      multiline
+                      onChange={updateListingDescription}
+                    />
+                    <TextField
+                      id="field-price"
+                      label="Price(CAD)"
+                      type="number"
+                      sx={{ m: "10px" }}
+                      rows={1}
+                      InputProps={{ inputProps: { min: 0 } }}
+                      onChange={updateListingPrice}
+                      error={!!priceError}
+                    />
+                    {priceError && (
+                      <FormHelperText error>{priceError}</FormHelperText>
+                    )}
+                  </FormControl>
+                  <Box sx={{ display: "flex" }}>
+                    <Button
+                      type="submit"
+                      variant="contained"
+                      sx={{
+                        display: "inline",
+                        mt: 2,
+                        backgroundColor: colors.martletplaceNavyBlue,
+                        "&:hover": {
+                          backgroundColor: colors.martletplaceBlueHover,
+                        },
+                        textTransform: "none",
+                        fontSize: "16px",
+                        padding: "10px 20px",
+                        margin: "10px",
+                      }}
+                      id={"submit-button"}
+                    >
+                      Create Listing
+                    </Button>
+                    <MultiFileUpload
+                      passedImages={listingImages}
+                      setPassedImages={setListingImages}
+                      multipleUpload={true}
+                      htmlForButton={buttonHTML}
+                      imageBinary={listingImageBinaries}
+                      setImageBinaries={setListingImageBinaries}
+                    />
+                  </Box>
+                </form>
+              </Box>
+            </Grid>
+            <Grid item lg={6} xs={12}>
+              <Box>
+                <Typography variant={"h5"} sx={{ paddingLeft: "20px" }}>
+                  Image Preview
+                </Typography>
+                <Box sx={{ padding: "10px" }}>
+                  {!isImageValid(listingImages[0]) ? (
+                    <Typography sx={{ paddingLeft: "10px" }} variant={"body2"}>
+                      No images uploaded yet
+                    </Typography>
+                  ) : (
+                    <Carousel imageURLs={listingImages} />
+                  )}
+                </Box>
+              </Box>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
+    </Container>
+  );
 };
 
 export default CreateListing;
