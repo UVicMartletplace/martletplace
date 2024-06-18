23542dd1ef39885fee317a80b8e9e18c94bb80b0
Scott <skenning128@gmail.com>
2024-06-18 01:36:42 -0700

    run npm format on mock utils

--- apps/frontend/src/MockUserUtils.tsx
+++ apps/frontend/src/MockUserUtils.tsx
@@ -1,15 +1,13 @@
 export interface User {
-    id: string;
-    username: string;
-    name: string;
-  }
-  
-  // Define a default user object
-  export const defaultUser: User = {
-    id: '123',
-    username: 'defaultUser',
-    name: 'Default User'
-  };
-  
-  export const getDefaultUser = (): User => defaultUser;
-  
+  id: string;
+  username: string;
+  name: string;
+}
+
+export const defaultUser: User = {
+  id: "123",
+  username: "defaultUser",
+  name: "Default User",
+};
+
+export const getDefaultUser = (): User => defaultUser;
