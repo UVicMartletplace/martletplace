61f36b4309376057806e2ce300aa1a688ccf953d
Anika Bookout <anbookout@gmail.com>
2024-06-18 17:07:57 -0700

    fixed global body style

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import ForgotPassword from "./pages/ForgotPassword";
