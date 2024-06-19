a550837caffcdb72ecc787f2d855b6c04ef474a3
Anika Bookout <anbookout@gmail.com>
2024-06-18 17:32:53 -0700

    Made more efficient, minor styling

--- apps/frontend/src/styles/pageStyles.tsx
+++ apps/frontend/src/styles/pageStyles.tsx
@@ -1,8 +1,8 @@
 import { colors } from "./colors";
 
 export const vars = Object.freeze({
-  pageHeaderHeight: "2rem",
-  messagesSendBoxHeight: "3rem",
+  pageHeaderHeight: "3rem",
+  messagesSendBoxHeight: "4rem",
 });
 
 export const useStyles = () => ({
@@ -166,7 +166,7 @@ export const useStyles = () => ({
   },
   // Messages page styles
   messagesBox: {
-    height: "100%",
+    height: `calc(100vh - ${vars.pageHeaderHeight})`,
   },
   messagesSidebar: {
     width: "10rem",
@@ -206,6 +206,7 @@ export const useStyles = () => ({
     padding: "0.5rem",
     maxWidth: "80vw",
     borderRadius: "10px",
+    textAlign: "right",
     borderBottomRightRadius: "2px",
     marginInline: "1rem",
   },
@@ -216,6 +217,7 @@ export const useStyles = () => ({
     padding: "0.5rem",
     maxWidth: "80vw",
     borderRadius: "10px",
+    textAlign: "left",
     borderBottomLeftRadius: "2px",
     marginInline: "1rem",
   },
