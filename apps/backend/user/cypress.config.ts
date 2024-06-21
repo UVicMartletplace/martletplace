c7bbceea15a6c9b3cf7af662eb612e713c732b22
Julian France <julian123france@gmail.com>
2024-06-21 16:39:59 -0700

    Babel removed from package, cypress config copied from listings service

--- apps/backend/user/cypress.config.ts
+++ apps/backend/user/cypress.config.ts
@@ -2,8 +2,11 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: 'http://localhost:8211',
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: false,
+    // setupNodeEvents(on, config) {
+    //   // implement node event listeners here
+    // },
   },
 });
