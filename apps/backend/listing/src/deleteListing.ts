d204aba71e3776e85602217b344238aea1fe6cf1
Brent Machado <65056763+bmachado18@users.noreply.github.com>
2024-06-20 17:11:29 -0700

    Brent/backend65 listing endpoints (#144)
    # Description
    Implemented each API's testing and functionality for listing
    microservice. This firstly involved configuring Cypress to the correct
    directory structure, then repeatedly writing tests equivalence class
    tests for an unimplemented API route, and after implementing the route
    until the tests pass. These endpoints defined within [the swagger UI
    under listings](http://market.lebl.ca/openapi/#/Listings/editListing),
    and within [the API list in the SRS
    doc](https://docs.google.com/document/d/1DNqexMBf4MpYPCxWsT3plUDAjDSbdJwUzw-UyZX-vDs/edit#heading=h.vf9vwfh56ne8).
    
    An additional module installed was pg-promise with the command ```npm
    install pg-promise```
    
    Keeping as a draft PR until all routes have implementations and tests. 
    - [x] GET /api/listing/{id}
    - [x] POST /api/listing
    - [x] DELETE /api/listing/{id}
    - [x] PATCH /api/listing/{id}
    
    Concerns:
    - In the API Agreement, GET listing includes the reviews of the listing,
    including the review information. Is that really necessary for every
    listing, or should it just return a list of review_id's attached to the
    listing? YES, since this api is mainly used by frontend and they user
    info need it on the listing page.
    - In the API Agreement, GET listing does not have a comment/message
    describing the error. Should this be updated?
    - in API Agreement, the post listing (Create) returns the newly created
    data. This violates REST principles, a POST should create and a GET
    should retrieve, right? So the POST should just return a message
    indicating success or not.
    
    Closes #65 
    
    ## How to Test
    There might be a better way to do this, but:
    1. Remove all docker images, and build the following images:
    ```
    docker-compose down
    
    docker-compose up --build -d listing database datalayer
    ```
    3. Go into exec of the listing service within docker, and the test
    suite:
    
    
    ![image](https://github.com/UVicMartletplace/martletplace/assets/65056763/6d893ef5-2537-44d5-8a55-faa43d365a0f)
    
    ```npm test```
    
    
    ## Checklist
    - [x] The code includes tests if relevant
    - [x] I have *actually* self-reviewed my changes and done QA
    <!-- Only check this off if you have actually done a self-review! DO NOT request any review from others until you have done your self-review! -->
    
    ---------
    
    Co-authored-by: Max Thomson <git@mnt.dev>
    Co-authored-by: GDeane <90437591+GDeane@users.noreply.github.com>
    Co-authored-by: sejal-agarwal <72957712+sejal-agarwal@users.noreply.github.com>
    Co-authored-by: Sejal Agarwal <sejalagarwal@Sejals-MacBook-Air.local>
    Co-authored-by: Sejal Agarwal <sejalagarwal@w134-87-173-149.wireless.uvic.ca>
    Co-authored-by: Caelum Dudek <54737724+CaelumD25@users.noreply.github.com>
    Co-authored-by: Kyle Stang <33678573+kylestang@users.noreply.github.com>

---
+++ apps/backend/listing/src/deleteListing.ts
@@ -0,0 +1,37 @@
+import { Request, Response } from "express";
+import { IDatabase } from "pg-promise";
+
+// DELETE /api/listing/:id - Delete a listing
+const deleteListing = async (
+  req: Request,
+  res: Response,
+  db: IDatabase<object>,
+) => {
+  //TODO: AUTHENTICATION
+  const { id } = req.params;
+
+  if (!id) {
+    console.log("request body empty");
+    return res.status(400).json({ error: "missing parameter in request" });
+  }
+
+  try {
+    const result = await db.result(
+      `DELETE FROM listings
+       WHERE listing_id = $1`,
+      [id],
+    );
+
+    if (result.rowCount === 0) {
+      console.log("listing not found");
+      return res.status(404).json({ error: "Listing not found" });
+    }
+
+    return res.status(200).json({ message: "Listing deleted successfully" });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ error: "Something went wrong" });
+  }
+};
+
+export { deleteListing };
