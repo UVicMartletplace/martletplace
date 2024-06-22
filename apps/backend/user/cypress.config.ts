870cd0695b6e9b62426e9e297e94cd3088174c18
Julian France <julian123france@gmail.com>
2024-06-21 23:48:28 -0700

    Copy Cypress setup from 'Brent/backend65 listing endpoints'

--- apps/backend/user/cypress.config.ts
+++ apps/backend/user/cypress.config.ts
@@ -2,11 +2,8 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost',
+    baseUrl: 'http://localhost:8211',
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     supportFile: false,
-    // setupNodeEvents(on, config) {
-    //   // implement node event listeners here
-    // },
   },
 });
