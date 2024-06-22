f8d818fd47ba8cae532175681bfd2225a6e55e95
Christian Bookout <christianmbookout@gmail.com>
2024-06-22 12:51:57 -0700

    Recommend on listing clicked (#196)
    # Description
    <!-- Please include a summary of the change(s) and which issue is being
    fixed. Please provide as much detail as possible. -->
    
    
    <!-- Replace `XXX` with the concerning issue number. The "#" links this
    PR to its relevant issue -->
    Closes #58 
    
    ## How to Test
    <!-- Provide some simple steps so that others can verify your
    implementation -->
    
    
    ## Checklist
    - [ ] The code includes tests if relevant
    - [ ] I have *actually* self-reviewed my changes and done QA
    <!-- Only check this off if you have actually done a self-review! DO NOT
    request any review from others until you have done your self-review! -->

--- apps/algorithm/recommend/src/training/run.py
+++ apps/algorithm/recommend/src/training/run.py
@@ -33,6 +33,17 @@ normalized_item_vectors = np.load("normalized_item_vectors.npy")
 data = pd.read_csv("processed_data.csv")
 
 
+def get_recommendations_by_items_clicked(
+    items_clicked, similarity_matrix, data, top_n=5
+):
+    indices = data.index[data["listingID"].isin(items_clicked)].tolist()
+    similatiries = np.mean(similarity_matrix[indices], axis=0)
+    recommendations = np.argsort(similatiries)[::-1]
+    # Remove items that have already been clicked
+    # Not sure if we want to do this or if we want to recommend stuff that has already been looked at?
+    # Needs to happen before taking the top_n, otherwise we aren't guaranteed to get top_n recommendations
+    recommendations = [i for i in recommendations if i not in indices]
+    return data.iloc[recommendations[:top_n]]
 def get_recommendations_from_search_terms(search_terms, data, top_n=5):
     search_tfidf_tensor = generate_tfidf_vector(search_terms, len(data))
     normalized_search_vectors = tf.nn.l2_normalize(search_tfidf_tensor, axis=1)
@@ -62,6 +73,16 @@ def get_recommendations(listing_id, similarity_matrix, data, top_n=5):
     return data.iloc[similar_listings_indices]
 
 
-listing_id = "B07DCVZ2GQ"
-recommended_listings = get_recommendations(listing_id, cosine_similarity_matrix, data)
+print("Getting recommendations for just one listing (soup)")
+listing_ids = ["B07DCVZ2GQ"]
+recommended_listings = get_recommendations_by_items_clicked(
+    listing_ids, cosine_similarity_matrix, data
+)
+print(recommended_listings)
+
+print("Getting recommendations based on items clicked (face products)")
+listing_ids = ["B07QDTZYSJ", "B072BGHNJ1", "B07H4LV2VS"]
+recommended_listings = get_recommendations_by_items_clicked(
+    listing_ids, cosine_similarity_matrix, data
+)
 print(recommended_listings)
