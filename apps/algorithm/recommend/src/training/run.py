f251c0e4c250d9fd5b89efe6652719e96461031c
Christian Bookout <christianmbookout@gmail.com>
2024-06-22 12:03:03 -0700

    Recommend on click

--- apps/algorithm/recommend/src/training/run.py
+++ apps/algorithm/recommend/src/training/run.py
@@ -4,15 +4,22 @@ import numpy as np
 cosine_similarity_matrix = np.load("cosine_similarity_matrix.npy")
 data = pd.read_csv("processed_data.csv")
 
+def get_recommendations_by_items_clicked(items_clicked, similarity_matrix, data, top_n=5):
+    indices = data.index[data["listingID"].isin(items_clicked)].tolist()
+    similatiries = np.mean(similarity_matrix[indices], axis=0)
+    recommendations = np.argsort(similatiries)[::-1]
+    # Remove items that have already been clicked
+    # Not sure if we want to do this or if we want to recommend stuff that has already been looked at?
+    # Needs to happen before taking the top_n, otherwise we aren't guaranteed to get top_n recommendations
+    recommendations = [i for i in recommendations if i not in indices]
+    return data.iloc[recommendations[:top_n]]
 
-def get_recommendations(listing_id, similarity_matrix, data, top_n=5):
-    idx = data.index[data["listingID"] == listing_id].tolist()[0]
-    similarity_scores = list(enumerate(similarity_matrix[idx]))
-    similarity_scores = sorted(similarity_scores, key=lambda x: x[1], reverse=True)
-    similar_listings_indices = [i[0] for i in similarity_scores[1 : top_n + 1]]
-    return data.iloc[similar_listings_indices]
+print("Getting recommendations for just one listing (soup)")
+listing_ids = ["B07DCVZ2GQ"]
+recommended_listings = get_recommendations_by_items_clicked(listing_ids, cosine_similarity_matrix, data)
+print(recommended_listings)
 
-
-listing_id = "B07DCVZ2GQ"
-recommended_listings = get_recommendations(listing_id, cosine_similarity_matrix, data)
+print("Getting recommendations based on items clicked (face products)")
+listing_ids = ["B07QDTZYSJ", "B072BGHNJ1", "B07H4LV2VS"]
+recommended_listings = get_recommendations_by_items_clicked(listing_ids, cosine_similarity_matrix, data)
 print(recommended_listings)
