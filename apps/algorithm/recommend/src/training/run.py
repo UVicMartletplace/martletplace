a4c98c8315f35e7776ba9a8a8446a5f96345fa66
Christian Bookout <christianmbookout@gmail.com>
2024-06-22 12:22:07 -0700

    ruff format

--- apps/algorithm/recommend/src/training/run.py
+++ apps/algorithm/recommend/src/training/run.py
@@ -4,7 +4,10 @@ import numpy as np
 cosine_similarity_matrix = np.load("cosine_similarity_matrix.npy")
 data = pd.read_csv("processed_data.csv")
 
-def get_recommendations_by_items_clicked(items_clicked, similarity_matrix, data, top_n=5):
+
+def get_recommendations_by_items_clicked(
+    items_clicked, similarity_matrix, data, top_n=5
+):
     indices = data.index[data["listingID"].isin(items_clicked)].tolist()
     similatiries = np.mean(similarity_matrix[indices], axis=0)
     recommendations = np.argsort(similatiries)[::-1]
@@ -14,12 +17,17 @@ def get_recommendations_by_items_clicked(items_clicked, similarity_matrix, data,
     recommendations = [i for i in recommendations if i not in indices]
     return data.iloc[recommendations[:top_n]]
 
+
 print("Getting recommendations for just one listing (soup)")
 listing_ids = ["B07DCVZ2GQ"]
-recommended_listings = get_recommendations_by_items_clicked(listing_ids, cosine_similarity_matrix, data)
+recommended_listings = get_recommendations_by_items_clicked(
+    listing_ids, cosine_similarity_matrix, data
+)
 print(recommended_listings)
 
 print("Getting recommendations based on items clicked (face products)")
 listing_ids = ["B07QDTZYSJ", "B072BGHNJ1", "B07H4LV2VS"]
-recommended_listings = get_recommendations_by_items_clicked(listing_ids, cosine_similarity_matrix, data)
+recommended_listings = get_recommendations_by_items_clicked(
+    listing_ids, cosine_similarity_matrix, data
+)
 print(recommended_listings)
