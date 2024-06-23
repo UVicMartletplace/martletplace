35a9e9e12b44e35eb8b20c95ec490f845fb39024
Christian Bookout <christianmbookout@gmail.com>
2024-06-23 14:08:59 -0700

    simple recommender tests

--- apps/algorithm/recommend/src/test_server.py
+++ apps/algorithm/recommend/src/test_server.py
@@ -1,2 +1,32 @@
-def test_addition():
-    assert 1 + 2 == 3
+from fastapi.testclient import TestClient
+
+from server import app
+
+client = TestClient(app)
+
+def test_get_recommendations():
+    response = client.get(
+        "/api/recommendations",
+        headers={"authorization": "1"},
+        params={"page": 1, "limit": 10},
+    )
+    assert response.status_code == 200
+    assert len(response.json()) == 10
+
+def test_get_recommendations_invalid_user():
+    response = client.get(
+        "/api/recommendations",
+        headers={"authorization": "abc"},
+        params={"page": 1, "limit": 10},
+    )
+    assert response.status_code == 404
+    assert response.json() == {"detail": "User not found: abc"}
+
+def test_get_recommendations_no_recommendations():
+    response = client.get(
+        "/api/recommendations",
+        headers={"authorization": "2"},
+        params={"page": 1, "limit": 0},
+    )
+    assert response.status_code == 200
+    assert response.json() == []
