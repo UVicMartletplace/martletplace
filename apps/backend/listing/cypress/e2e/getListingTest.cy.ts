970bed8f0e48ddf0280ddb2e2d00d6e818633da3
Julian France <julian123france@gmail.com>
2024-06-24 17:19:19 -0700

    revert "Fixed getListingTest to use the new Amazon dataset's DB listings."
    This reverts commit b04780ad2314cb8b2dfa451bc75cba522d5db13d.

--- apps/backend/listing/cypress/e2e/getListingTest.cy.ts
+++ apps/backend/listing/cypress/e2e/getListingTest.cy.ts
@@ -7,16 +7,16 @@ describe('Get Listing by ID Endpoint', () => {
       url: `${baseUrl}/1`, // assuming listing with ID 1 exists
       body: {
         user_location: {
-          latitude: 48.4310,  // same as listing 1 latitude
-          longitude: 123.5920 // same as listing 1 longitude
+          latitude: 40.7128,  // same as listing 1 latitude
+          longitude: -74.0060 // same as listing 1 longitude
         }
       }
     }).then((response) => {
       expect(response.status).to.eq(200);
       expect(response.body).to.have.property('listingID', 1);
-      expect(response.body).to.have.property('title', ' Lee posh Lactic Acid 60% Anti ageing Pigmentation Removing Glow Peel ');
-      expect(response.body).to.have.property('description', 'PROFESSIONAL GRADE Face Peel: this peel stimulates collagen production, reducing the appearance of wrinkles, fine lines, and hyper pigmentation in the skin by increasing cell regeneration Highly effective professional strength superficial solution Read direction for use on bottle of product & if any query call customer care');
-      expect(response.body).to.have.property('price', 13);
+      expect(response.body).to.have.property('title', 'Listing One');
+      expect(response.body).to.have.property('description', 'Description for listing one');
+      expect(response.body).to.have.property('price', 100);
       expect(response.body).to.have.property('status', 'AVAILABLE');
       expect(response.body).to.have.property('dateCreated');
       expect(response.body).to.have.property('dateModified');
