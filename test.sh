61e30e551792669bcc021e767f1fbd82d371fd81
Toby Murray <tobystefan@gmail.com>
2024-06-24 20:43:59 -0700

    Add prefix flag

--- test.sh
+++ test.sh
@@ -2,14 +2,15 @@
 
 docker compose up
 
-docker compose exec user npm run test
-docker compose exec listing npm run test
-docker compose exec review npm run test
-docker compose exec message npm run test
-docker compose exec lib npm run test
+docker compose exec user npm --prefix ../lib install
+docker compose exec user npm --prefix ../lib run test:ci
+docker compose exec listing npm run test:ci
+docker compose exec review npm run test:ci
+docker compose exec message npm run test:ci
+docker compose exec lib npm run test:ci
 docker compose exec search pytest
 docker compose exec recommend pytest
 npm ci
-npm run test
+npm run testa
 
 docker compose down
