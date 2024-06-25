47c5eddcae569cc2a211c9aa37669836cf20b3d5
Brent Machado <65056763+bmachado18@users.noreply.github.com>
2024-06-25 14:33:08 -0700

    Brent/backend66 review endpoints (#202)
    # Description
    Updated the testing framework to cypress. Added tests for TDD for each
    CRUD API for reviews. Implemented each endpoint, and ensured passing in
    CI. Note that the getReview endpoint is not in the API spec, but I
    included it anyway for full CRUD.
    
    I am just waiting on the OpenAPI spec PR to be approved so I can
    progress from a draft PR. See
    https://github.com/matt-lebl/uvic-market-openapi/pull/18.
    
    Closes #66 
    
    ## How to Test
    ```docker-compose down -v```
    ```docker-compose up --build -d review database datalayer proxy```
    ```docker exec -it martletplace_review bash```
    ```npm test```
    
    
    ## Checklist
    - [x] The code includes tests if relevant
    - [x] I have *actually* self-reviewed my changes and done QA
    <!-- Only check this off if you have actually done a self-review! DO NOT
    request any review from others until you have done your self-review! -->
    
    ---------
    
    Co-authored-by: Max Thomson <git@mnt.dev>

---
+++ apps/backend/review/src/createReview.ts
@@ -0,0 +1,57 @@
+import { Request, Response } from "express";
+import { IDatabase } from "pg-promise";
+
+// POST /api/reviews - Create a new review
+const createReview = async (
+  req: Request,
+  res: Response,
+  db: IDatabase<object>,
+) => {
+  // TODO: AUTHENTICATION
+  const userID = 1; // placeholder for authenticated user ID
+  const { stars, comment, listingID } = req.body;
+
+  if (!stars || !comment || !listingID) {
+    console.error("missing parameter in request");
+    return res.status(400).json({ error: "missing parameter in request" });
+  }
+
+  if (stars < 1 || stars > 5) {
+    console.error("invalid rating value");
+    return res.status(400).json({ error: "invalid rating value" });
+  }
+
+  try {
+    const createdReview = await db.one(
+      `INSERT INTO reviews (listing_id, user_id, review, rating_value)
+       VALUES ($1, $2, $3, $4)
+       RETURNING *`,
+      [listingID, userID, comment, stars],
+    );
+
+    const reviewerProfile = await db.one(
+      `SELECT user_id AS "userID", username, name
+       FROM users
+       WHERE user_id = $1`,
+      [userID],
+    );
+
+    const responseReview = {
+      review_id: createdReview.review_id,
+      reviewerName: reviewerProfile.name,
+      stars: createdReview.rating_value,
+      comment: createdReview.review,
+      userID: reviewerProfile.userID,
+      listingID: createdReview.listing_id,
+      dateCreated: createdReview.created_at,
+      dateModified: createdReview.modified_at,
+    };
+
+    return res.status(201).json(responseReview);
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ error: "Something went wrong" });
+  }
+};
+
+export { createReview };
