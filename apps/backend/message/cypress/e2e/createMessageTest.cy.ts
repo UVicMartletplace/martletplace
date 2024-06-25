dac604e5a0d81f2baa078e549a02f954eff944cf
Anika Bookout <anbookout@gmail.com>
2024-06-25 10:22:01 -0700

    Messages cypress config + basic tests

---
+++ apps/backend/message/cypress/e2e/createMessageTest.cy.ts
@@ -0,0 +1,35 @@
+describe("Create Message", () => {
+  const baseUrl = "0.0.0.0:/api/messages/";
+
+  it("should successfully create a new message", () => {
+    const content = "hi i want to buy your couch";
+    cy.request({
+      method: "POST",
+      url: baseUrl,
+      body: {
+        listing_id: "1",
+        receiver_id: "2",
+        content: content,
+      },
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+      expect(response.body).to.have.property("content", content);
+      expect(response.body).to.have.property("listing_id", "1");
+      expect(response.body).to.have.property("receiver_id", "2");
+    });
+  });
+
+  it("should fail to create a new message with missing parameters", () => {
+    cy.request({
+      method: "POST",
+      url: baseUrl,
+      body: {
+        content: "hi",
+      },
+      failOnStatusCode: false,
+    }).then((response) => {
+      expect(response.status).to.eq(400);
+      expect(response.body).to.have.property("error");
+    });
+  });
+});
