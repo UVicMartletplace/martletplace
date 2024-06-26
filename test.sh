b55207187bf660f7c8b1770e774b91676bb6892f
Anika Bookout <anbookout@gmail.com>
2024-06-26 13:35:25 -0700

    attempting to make lib tests pass

--- test.sh
+++ test.sh
@@ -5,8 +5,7 @@ docker rm -vf martletplace_database
 docker compose up --build -d
 
 echo 'Testing backend services...'
-# NOTE: can't test lib like this, it would need to have its own server (cypress moment)
-# docker compose exec user bash -c 'cd ../lib && npm install && npm run test:ci'
+docker compose exec user bash -c 'cd ../lib && npm install && npm run test:ci'
 docker compose exec user npm run test:ci
 docker compose exec listing npm run test:ci
 docker compose exec review npm run test:ci
