6c0aea0e61827a4c339dcc6e41e7ef4a1efb68b2
ChloeZacharias <chloe.m.zacharias@gmail.com>
2024-06-26 13:45:49 -0700

    Prettier

--- apps/frontend/src/components/filters.tsx
+++ apps/frontend/src/components/filters.tsx
@@ -51,11 +51,11 @@ const Filters = ({ filters, onFilterChange }: FiltersProps) => {
       setPriceError("");
     } else if (!regex.test(event.target.value)) {
       setPriceError(
-        "This price is not valid, please make sure the value is positive and in the form xx.xx"
+        "This price is not valid, please make sure the value is positive and in the form xx.xx",
       );
     } else if (maxPrice !== null && newMinPrice > maxPrice) {
       setPriceError(
-        "This price is not valid, please make sure the min price is less than the max price"
+        "This price is not valid, please make sure the min price is less than the max price",
       );
     } else {
       setPriceError("");
@@ -72,11 +72,11 @@ const Filters = ({ filters, onFilterChange }: FiltersProps) => {
       setPriceError("");
     } else if (!regex.test(event.target.value)) {
       setPriceError(
-        "This price is not valid, please make sure the value is positive and in the form xx.xx"
+        "This price is not valid, please make sure the value is positive and in the form xx.xx",
       );
     } else if (minPrice !== null && newMaxPrice < minPrice) {
       setPriceError(
-        "This price is not valid, please make sure the min price is less than the max price"
+        "This price is not valid, please make sure the min price is less than the max price",
       );
     } else {
       setPriceError("");
