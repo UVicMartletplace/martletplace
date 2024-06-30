c6d0e693a1f3f90bee8b901cb1a7014f39255ae3
Caelum Dudek <caelumdudek25@gmail.com>
2024-06-30 13:22:17 -0700

    Spinner loading now indicates network errors

--- apps/frontend/src/components/Spinner.tsx
+++ apps/frontend/src/components/Spinner.tsx
@@ -1,27 +1,65 @@
 import CircularProgress from "@mui/material/CircularProgress";
 import { Grid, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
 
 interface Props {
   text: string;
 }
 
 export default function Spinner({ text }: Props) {
+  const [isAlive, setIsAlive] = useState<boolean>(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsAlive(false);
+    }, 40000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <Grid
-      gap={2}
-      container
-      direction="column"
-      alignItems="center"
-      justifyContent="center"
-      sx={{ marginTop: "40px" }}
-    >
-      <Grid item>
-        <CircularProgress size="40px" />
-      </Grid>
-      <Grid item sx={{ textAlign: "center" }}>
-        <Typography variant="h5">Hang with us</Typography>
-        <Typography variant="h6">{text}</Typography>
-      </Grid>
-    </Grid>
+    <>
+      {isAlive ? (
+        <Grid
+          gap={2}
+          container
+          direction="column"
+          alignItems="center"
+          justifyContent="center"
+          sx={{ marginTop: "40px" }}
+        >
+          <Grid item>
+            <CircularProgress size="40px" />
+          </Grid>
+          <Grid item sx={{ textAlign: "center" }}>
+            <Typography variant="h5">Hang with us</Typography>
+            <Typography variant="h6">{text}</Typography>
+          </Grid>
+        </Grid>
+      ) : (
+        <Grid
+          gap={2}
+          container
+          direction="column"
+          alignItems="center"
+          justifyContent="center"
+          sx={{ marginTop: "40px" }}
+        >
+          <Grid item>
+            <CircularProgress
+              variant="determinate"
+              value={100}
+              size="40px"
+              sx={{ color: "red" }}
+            />
+          </Grid>
+          <Grid item sx={{ textAlign: "center" }}>
+            <Typography variant="h5">Network Error</Typography>
+            <Typography variant="h6">
+              Please try again some other time
+            </Typography>
+          </Grid>
+        </Grid>
+      )}
+    </>
   );
 }
