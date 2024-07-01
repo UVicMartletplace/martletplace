92ea6479ea06cc40fde413163fd320141bb3c13c
Sejal Agarwal <sejalagarwal@Sejals-MacBook-Air.local>
2024-06-30 17:55:46 -0700

    fix homepage tests

--- apps/frontend/tests/component_tests/Homepage.cy.tsx
+++ apps/frontend/tests/component_tests/Homepage.cy.tsx
@@ -1,5 +1,6 @@
-import Homepage from "../../src/pages/Homepage";
 import { MemoryRouter } from "react-router-dom";
+import Homepage from "../../src/pages/Homepage";
+import UserProvider from "../../src/contexts/UserProvider"; // Adjust the path to your UserProvider
 
 describe("<Homepage />", () => {
   const recomendationsObject = [
@@ -102,9 +103,11 @@ describe("<Homepage />", () => {
     }).as("recomendations");
 
     cy.mount(
-      <MemoryRouter>
-        <Homepage />
-      </MemoryRouter>
+      <UserProvider>
+        <MemoryRouter>
+          <Homepage />
+        </MemoryRouter>
+      </UserProvider>
     );
     cy.viewport(1800, 720);
   });
