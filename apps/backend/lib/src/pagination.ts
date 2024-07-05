e1537d3a96da00a43c9e54cc30989178de25143b
Anika Bookout <anbookout@gmail.com>
2024-07-04 17:52:33 -0700

    Wrote pagination middleware tests

--- apps/backend/lib/src/pagination.ts
+++ apps/backend/lib/src/pagination.ts
@@ -5,7 +5,7 @@ import { Request, Response, NextFunction } from "express";
 export const usePagination = (
   req: Request,
   res: Response,
-  next: NextFunction
+  next: NextFunction,
 ) => {
   const num_items = parseInt(req.query.num_items as string) || 10;
   const offset = parseInt(req.query.offset as string) || 0;
