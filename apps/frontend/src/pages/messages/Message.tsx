84fd0b618bd01a52e2cb6e480ea4b38accabc1d5
Anika Bookout <anbookout@gmail.com>
2024-07-05 17:53:04 -0700

    Fetch more messages fix for frontend

--- apps/frontend/src/pages/messages/Message.tsx
+++ apps/frontend/src/pages/messages/Message.tsx
@@ -1,17 +1,21 @@
 import { Box } from "@mui/material";
 import { useStyles } from "../../styles/pageStyles";
 import { MessageType } from "../../types";
-
-const user_id = "5"; //TODO: get from auth (in a Context or something)
+import useUser from "../../hooks/useUser";
 
 export type MessageProps = {
   message: MessageType;
 };
 export const Message = ({ message }: MessageProps) => {
   const s = useStyles();
+  const user = useUser();
   return (
     <Box
-      sx={message.sender_id == user_id ? s.messageFromUser : s.messageFromOther}
+      sx={
+        message.sender_id == user.user?.id
+          ? s.messageFromUser
+          : s.messageFromOther
+      }
     >
       {message.message_body}
     </Box>
