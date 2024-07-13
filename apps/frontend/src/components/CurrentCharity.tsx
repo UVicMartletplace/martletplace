db3b92c601f9faebf86de2727d6014de104051af
Sejal Agarwal <sejalagarwal@Sejals-MacBook-Air.local>
2024-07-13 13:57:57 -0700

    run formatter

--- apps/frontend/src/components/CurrentCharity.tsx
+++ apps/frontend/src/components/CurrentCharity.tsx
@@ -22,7 +22,7 @@ const CurrentCharity = ({ charity }: CurrentCharityProps) => {
       } else {
         const days = Math.floor(distance / (1000 * 60 * 60 * 24));
         const hours = Math.floor(
-          (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+          (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
         );
         const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         const seconds = Math.floor((distance % (1000 * 60)) / 1000);
