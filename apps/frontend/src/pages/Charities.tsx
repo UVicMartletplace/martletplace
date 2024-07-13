9d27bd292e8359b6b188ce018ed0d6326d0cf8b4
Sejal Agarwal <sejalagarwal@Sejals-MacBook-Air.local>
2024-07-13 10:44:45 -0700

    run formatter

--- apps/frontend/src/pages/Charities.tsx
+++ apps/frontend/src/pages/Charities.tsx
@@ -75,10 +75,10 @@ const Charities = () => {
         } else {
           const days = Math.floor(distance / (1000 * 60 * 60 * 24));
           const hours = Math.floor(
-            (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+            (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
           );
           const minutes = Math.floor(
-            (distance % (1000 * 60 * 60)) / (1000 * 60)
+            (distance % (1000 * 60 * 60)) / (1000 * 60),
           );
           const seconds = Math.floor((distance % (1000 * 60)) / 1000);
           setTimeRemaining(`${days}d ${hours}h ${minutes}m ${seconds}s`);
