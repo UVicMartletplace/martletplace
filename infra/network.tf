ac94db79f4470d918764f1c17105f189b8e3c372
Kyle Stang <33678573+kylestang@users.noreply.github.com>
2024-07-23 18:03:41 -0700

    [Resops] Add opensearch (#482)
    # Description
    
    
    Closes #413 
    
    ## How to Test
    
    
    ## Checklist
    - [ ] The code includes tests if relevant
    - [ ] I have *actually* self-reviewed my changes and done QA

--- infra/network.tf
+++ infra/network.tf
@@ -1,7 +1,13 @@
 data "aws_availability_zones" "available" {}
 
+data "aws_region" "current" {}
+
+data "aws_caller_identity" "current" {}
+
 resource "aws_vpc" "main" {
-  cidr_block = "10.10.0.0/16"
+  cidr_block           = "10.10.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 }
 
 resource "aws_subnet" "public" {
