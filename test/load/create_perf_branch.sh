9d9327e399c1e82d192629bdb8d07fb334fc6ddf
Max Thomson <git@mnt.dev>
2024-07-23 18:52:41 -0700

    Delete perf branches thing

--- test/load/create_perf_branch.sh
+++ test/load/create_perf_branch.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 git stash --include-untracked
 CURRENT_HEAD=$(git rev-parse HEAD)
 
-git merge max/speed-up-search max/search-database-connection resops/productionize-py-services max/optimize-db-indexes max/increase-pgbouncer-conn-limit max/lower-backend-logging max/test-remove-triggers
+git merge #Add all perf branches here
 
-git reset --soft $CURRENT_HEAD
+git reset --soft "$CURRENT_HEAD"
 git stash pop
