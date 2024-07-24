c95c749242cff6acb46308c96504559be5e65794
Max Thomson <git@mnt.dev>
2024-07-23 18:55:06 -0700

    [Loadtest] Add transaction ramp to relieve pressure on user service (#450)
    # Description
    The "startup time" is somewhat fake as it resets all
    transactions/scenarios when the real test starts running. Meaning that
    the user service gets absolutely pounded by every goose at once. Using
    the test_plan lets us start the transactions slowly and ramp up (with no
    sudden restarts). This change got RPS +30% and the P50,95 on User -80%
    
    Closes #449
    
    ## Checklist
    - [ ] The code includes tests if relevant
    - [x] I have *actually* self-reviewed my changes and done QA

---
+++ test/load/local.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Ensure that all services are fresh & the DB isn't accruing crud from previous loadtests
+docker compose restart user listing review message recommend search datalayer
+docker compose down -v database
+docker compose up --build --wait database
+docker compose restart user listing review message recommend search datalayer
+
+sleep 10
+
+cargo run --release
