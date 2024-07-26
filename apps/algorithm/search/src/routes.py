fe2c8ddf30df021bb55e3a5dff7065bba66762fd
Emila Suska <emila.suska@gmail.com>
2024-07-26 15:17:20 -0700

    added fuzzy search

--- apps/algorithm/search/src/routes.py
+++ apps/algorithm/search/src/routes.py
@@ -38,8 +38,21 @@ async def search(
 
     if searchType == "LISTINGS":
         must_conditions = [
-            {"multi_match": {"query": query, "fields": ["title", "description"]}},
-            {"match": {"status": status}},
+            {
+                "multi_match": {
+                    "query": query,
+                    "fields": [
+                        "title",
+                        "description"
+                    ],
+                    "fuzziness": "AUTO"
+                }
+            },
+            {
+                "match": {
+                    "status": status
+                }
+            }
         ]
     elif searchType == "USERS":
         must_conditions = [
@@ -52,7 +65,19 @@ async def search(
     search_body: Dict[str, Any] = {
         "from": (page - 1) * limit,
         "size": limit,
-        "query": {"bool": {"must": must_conditions, "filter": []}},
+        "query": {
+            "bool": {
+                "must": must_conditions,
+                "filter": [
+                    {
+                        "geo_distance": {
+                            "distance": DISTANCE_TO_SEARCH_WITHIN,
+                            "location": {"lat": latitude, "lon": longitude},
+                        }
+                    }
+                ]
+            },
+        },
         "sort": [],
     }
 
@@ -63,15 +88,6 @@ async def search(
         if maxPrice is not None:
             price_range["lte"] = maxPrice
         search_body["query"]["bool"]["filter"].append({"range": {"price": price_range}})
-
-    search_body["query"]["bool"]["filter"].append(
-        {
-            "geo_distance": {
-                "distance": DISTANCE_TO_SEARCH_WITHIN,
-                "location": {"lat": latitude, "lon": longitude},
-            }
-        }
-    )
 
     if "DISTANCE" in sort:
         search_body["sort"].append(
