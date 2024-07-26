9e10e064935390452aa9913e3a6ec504e5c93e9b
Arun Alex <arunalex62@gmail.com>
2024-07-25 23:51:16 -0700

    Modified ListingSummary API model to conform to API spec.

--- apps/algorithm/recommend/src/api_models.py
+++ apps/algorithm/recommend/src/api_models.py
@@ -4,36 +4,32 @@ from datetime import datetime, timezone
 
 
 class ListingSummary(BaseModel):
-    listingID: Optional[str] = Field(
+    listingID: str = Field(
         default=None, description="The primary key for the listing."
     )
-    sellerID: str = Field(
+    sellerID: Optional[str] = Field(
         ..., description="The ID of the seller, foreign key to users."
     )
-    sellerName: str = Field(..., description="The name of the seller.")
-    buyerID: Optional[int] = Field(
-        default=None, description="The ID of the buyer, if any."
-    )
+    sellerName: Optional[str] = Field(..., description="The name of the seller.")
     title: str = Field(..., description="The title of the listing.")
     price: int = Field(..., description="The price of the item listed.")
     location: dict = Field(
         default={}, description="A dictionary containing location details."
     )
-    status: str = Field(..., description="The status of the listing.")
     description: Optional[str] = Field(
-        default=None, description="A detailed description of the listing."
+        default="No description set", description="A detailed description of the listing."
     )
-    imageUrl: str = Field(
-        default=...,
+    imageUrl: Optional[str] = Field(
+        default=None,
         description="The URL for the first image associated with the listing.",
     )
     dateCreated: datetime = Field(
         default_factory=lambda: datetime.now(timezone.utc),
         description="The UTC timestamp when the listing was created.",
     )
-    modified_at: datetime = Field(
-        default_factory=lambda: datetime.now(timezone.utc),
-        description="The UTC timestamp when the listing was last modified.",
+    charityID: Optional[str] = Field(
+        default=None,
+        description="The ID of the charity the item is associated with (if any).",
     )
 
 
