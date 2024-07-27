0cd47bb57b84ee579e2e42c5acc20c09a49df15d
GDeane <90437591+GDeane@users.noreply.github.com>
2024-07-27 14:33:20 -0700

    Gavin/playing with lat and lon stuff (#511)
    # Description
    Changing latitude and longitude as follows:
    
    (previously)
    "location": { "latitude": 45.4215, "longitude": -75.6972 },
    
    (after change)
    "location": { "lat": 40.7128, "lon": -74.006 }.
    
    This makes the frontend search by location within 5km of input work
    correctly, and sorting by distance ascending by descending.
    
    
    ![image](https://github.com/user-attachments/assets/62dfc3b5-09cb-4f18-bbd0-52aa0e18c42c)
    
    ![image](https://github.com/user-attachments/assets/11fd35d6-9943-4179-a585-aac595740bbb)
    
    I tested the app manually and ran backend tests and couldn't find
    anything broken by this.
    
    Closes #260 
    
    ## How to Test
    - Build the app, log in, search listings, and sort by location (it
    should change the order of listings)
    - Also try creating a listing at your current location, then search for
    it and sort by ascending location (it should appear first)
    - Once the application is built, run `docker exec -it
    martletplace_search bash` in a different terminal, then run `pytest
    src/test_server.py`
    
    ## Checklist
    - [ ] The code includes tests if relevant
    - [ ] I have *actually* self-reviewed my changes and done QA
    
    ---------
    
    Co-authored-by: emilasuska <43764460+emilasuska@users.noreply.github.com>

--- apps/data/database/populatedb.sql
+++ apps/data/database/populatedb.sql
@@ -7,11 +7,11 @@ INSERT INTO users (username, email, password, totp_secret, name, bio, profile_pi
 -- ALL PASSWORDS: Letmeinfortesting1!
 
 INSERT INTO listings (seller_id, buyer_id, title, description, price, location, status, image_urls) VALUES
-(1, NULL, 'Listing One', 'Description for listing one', 100, ROW(40.7128, -74.0060), 'AVAILABLE', ARRAY['https://api.dicebear.com/8.x/bottts/svg?seed=Jasper', 'https://api.dicebear.com/8.x/bottts/svg?seed=Bella', 'https://api.dicebear.com/8.x/bottts/svg?seed=Bella']),
-(2, 3, 'Listing Two', 'Description for listing two', 200, ROW(34.0522, -118.2437), 'SOLD', ARRAY['https://api.dicebear.com/8.x/bottts/svg?seed=Lucy']),
-(3, 4, 'Listing Three', 'Description for listing three', 300, ROW(41.8781, -87.6298), 'REMOVED', ARRAY['https://api.dicebear.com/8.x/bottts/svg?seed=Angel']),
-(4, NULL, 'Listing Four', 'Description for listing four', 400, ROW(29.7604, -95.3698), 'AVAILABLE', ARRAY['https://api.dicebear.com/8.x/bottts/svg?seed=Casper']),
-(5, NULL, 'Listing Five', 'Description for listing five', 500, ROW(51.5074, -0.1278), 'AVAILABLE', ARRAY['https://api.dicebear.com/8.x/bottts/svg?seed=Kiki']);
+(1, NULL, 'Listing One', 'Description for listing one', 100, ROW(48.48256, -123.3234215), 'AVAILABLE', ARRAY['https://api.dicebear.com/8.x/bottts/svg?seed=Jasper', 'https://api.dicebear.com/8.x/bottts/svg?seed=Bella', 'https://api.dicebear.com/8.x/bottts/svg?seed=Bella']),
+(2, 3, 'Listing Two', 'Description for listing two', 200, ROW(48.4835652, -123.3234215), 'SOLD', ARRAY['https://api.dicebear.com/8.x/bottts/svg?seed=Lucy']),
+(3, 4, 'Listing Three', 'Description for listing three', 300, ROW(48.4845652, -123.3234215), 'REMOVED', ARRAY['https://api.dicebear.com/8.x/bottts/svg?seed=Angel']),
+(4, NULL, 'Listing Four', 'Description for listing four', 400, ROW(48.4815652, -123.3234215), 'AVAILABLE', ARRAY['https://api.dicebear.com/8.x/bottts/svg?seed=Casper']),
+(5, NULL, 'Listing Five', 'Description for listing five', 500, ROW(48.4805652, -123.3234215), 'AVAILABLE', ARRAY['https://api.dicebear.com/8.x/bottts/svg?seed=Kiki']);
 
 INSERT INTO messages (sender_id, receiver_id, listing_id, message_body) VALUES
 (2, 1, 1, 'You still offering this???'),
