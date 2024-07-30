a70010f1ba67a2a101dad4cd27d264e40918b7c9
Max Thomson <git@mnt.dev>
2024-07-30 15:12:23 -0700

    [Infra] Host goose on AWS (#542)
    # Description
    
    
    Closes #540
    
    ## How to Test
    
    
    ## Checklist
    - [ ] The code includes tests if relevant
    - [ ] I have *actually* self-reviewed my changes and done QA

--- infra/push-containers.sh
+++ infra/push-containers.sh
@@ -16,7 +16,7 @@ push() {
   docker push "${REPO_PATH}/${image}:${VER_TAG}"
 }
 
-for item in user listing review message charity search recommend frontend pgsync collector; do
+for item in user listing review message charity search recommend frontend pgsync collector loadtest; do
   push $item
 done
 
