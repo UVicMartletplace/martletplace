3c77376685aab3d13f5cd29848eed4962b795550
Kyle Stang <33678573+kylestang@users.noreply.github.com>
2024-07-30 16:36:57 -0700

    [datalayer] add image auth

--- apps/data/datalayer/src/main.rs
+++ apps/data/datalayer/src/main.rs
@@ -3,16 +3,19 @@ use std::future::IntoFuture;
 use elastic::{elastic_endpoint, elastic_router};
 use email::{email_endpoint, email_router};
 use image::image_router;
+use jwt::jwt_key;
 
 mod elastic;
 mod email;
 mod image;
+mod jwt;
 
 #[tokio::main]
 async fn main() {
     // Check that the env vars are set
     elastic_endpoint();
     email_endpoint();
+    jwt_key();
 
     let elastic_listener = tokio::net::TcpListener::bind("0.0.0.0:8301").await.unwrap();
     let email_listener = tokio::net::TcpListener::bind("0.0.0.0:8302").await.unwrap();
