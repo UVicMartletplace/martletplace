272e5c979e41a163988b4d4b0b7ee2bf56613bd1
Max Thomson <git@mnt.dev>
2024-07-30 12:18:03 -0700

    [Infra] Host Charity service (#537)
    # Description
    
    
    Closes #514
    
    ## How to Test
    
    
    ## Checklist
    - [ ] The code includes tests if relevant
    - [ ] I have *actually* self-reviewed my changes and done QA

--- infra/ecs-services.tf
+++ infra/ecs-services.tf
@@ -176,6 +176,33 @@ module "message" {
   health_check_path  = var.health_check_path
 }
 
+module "charity" {
+  source = "./ecs/"
+
+  app_name     = "charity"
+  app_image    = format("%s:%s", aws_ecr_repository.main["charity"].repository_url, var.app_version)
+  app_port     = 8215
+  app_route    = "/api/charities*"
+  app_priority = 93
+
+  environment = concat(local.base_environment, [])
+  secrets     = concat(local.base_secrets, [])
+
+  fargate_cpu    = var.fargate_cpu
+  fargate_memory = var.fargate_memory
+  app_count      = var.app_count
+
+  ecs_cluster        = aws_ecs_cluster.main
+  alb_id             = aws_alb.main.id
+  lb_port            = var.lb_port
+  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
+  task_role_arn      = aws_iam_role.ecs_task_role.arn
+  vpc_id             = aws_vpc.main.id
+  security_group_id  = aws_security_group.ecs_tasks.id
+  subnet_ids         = aws_subnet.public.*.id
+  health_check_path  = var.health_check_path
+}
+
 module "search" {
   source = "./ecs/"
 
