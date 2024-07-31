7b80d12d4f5b7af6a8445b6e0554c33936fd0384
Kyle Stang <33678573+kylestang@users.noreply.github.com>
2024-07-30 17:53:44 -0700

    [loadtest] Fix POST listing (#560)
    # Description
    
    
    Closes #559 
    
    ## How to Test
    
    
    ## Checklist
    - [ ] The code includes tests if relevant
    - [ ] I have *actually* self-reviewed my changes and done QA

--- test/load/local.sh
+++ test/load/local.sh
@@ -2,10 +2,10 @@
 set -euo pipefail
 
 # Ensure that all services are fresh & the DB isn't accruing crud from previous loadtests
-docker compose restart user listing review message recommend search datalayer
+docker compose restart user listing review message recommend search datalayer charity
 docker compose down -v database
 docker compose up --build --wait database
-docker compose restart user listing review message recommend search datalayer
+docker compose restart user listing review message recommend search datalayer charity
 
 sleep 10
 
